87e372ffbe9a7c95c1c1abe1b93db730bf6d87e1
Egor <EgorUlybin@MacBook-Pro.local>
2022-03-16 13:26:00 -0400

    Updates

---
+++ precache-manifest.319cc084e5f442dcdd492732c4f04386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c0dab9207e2905088e5e51fca37ab4",
+    "url": "/httpRequests14-react-app/index.html"
+  },
+  {
+    "revision": "1c09d4e2fec9534b4275",
+    "url": "/httpRequests14-react-app/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/httpRequests14-react-app/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/httpRequests14-react-app/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c09d4e2fec9534b4275",
+    "url": "/httpRequests14-react-app/static/js/main.8d6571de.chunk.js"
+  },
+  {
+    "revision": "97554d739c353b961d78",
+    "url": "/httpRequests14-react-app/static/js/runtime-main.a468757b.js"
+  }
+]);
